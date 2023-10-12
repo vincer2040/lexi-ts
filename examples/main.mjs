53beccf4a170e4e3e04065a0ad6ad3ef153731b1
boreddad <ronnebaumfinances@gmail.com>
2023-10-12 12:35:07 -0400

    chore: add examples

---
+++ examples/main.mjs
@@ -0,0 +1,15 @@
+import Lexi from "../dist/lexi.mjs";
+
+let client = new Lexi("127.0.0.1", 6969);
+
+await client.connect();
+
+let x = client.multi()
+    .addSet("vince", "is cool")
+    .addSet("yes", "no");
+
+let v = await x.done();
+
+console.log(v);
+
+client.close();
