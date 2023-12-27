06f9471e1a4dbc2ad413020202b7c6ebc084f4df
boreddad <ronnebaumfinances@gmail.com>
2023-12-27 11:24:39 -0500

    feat: dynamic buffer

---
+++ src/dynamicBuffer.ts
@@ -0,0 +1,51 @@
+const DYANMIC_BUFFER_INITIAL_CAPACITY = 32
+
+export class DynamicBuffer {
+    private buffer: Buffer;
+    private len: number;
+    private cap: number;
+    constructor(capacity?: number) {
+        this.len = 0;
+        if (capacity) {
+            this.cap = capacity
+        } else {
+            this.cap = DYANMIC_BUFFER_INITIAL_CAPACITY;
+        }
+        this.buffer = Buffer.alloc(this.cap);
+    }
+
+    public pushChar(char: number): void {
+        if (this.len >= this.cap) {
+            this.reallocBuffer(1);
+        }
+        this.buffer[this.len] = char;
+        this.len++;
+    }
+
+    public out(): Buffer {
+        return this.buffer;
+    }
+
+    public reset(): void {
+        this.buffer.fill(0);
+        this.len = 0;
+    }
+
+    public pushString(string: string): void {
+        const stringLength = string.length;
+        const needed = this.len + stringLength;
+        if (needed >= this.cap) {
+            this.reallocBuffer(stringLength);
+        }
+        this.buffer.write(string, this.len);
+        this.len += stringLength;
+    }
+
+    private reallocBuffer(needed: number): void {
+        const capacity = (this.cap * 2) + needed;
+        const newBuffer = Buffer.alloc(capacity);
+        this.buffer.copy(newBuffer);
+        this.cap = capacity;
+        this.buffer = newBuffer;
+    }
+}
