43d3f595ec634eb309d1759dcab59c554594a4ae
vincer2040 <ronnebaumfinances@gmail.com>
2024-04-30 14:12:56 -0400

    chore: change addString to addBulkString

--- tests/builder.spec.ts
+++ tests/builder.spec.ts
@@ -9,7 +9,7 @@ type IntTest = {
 describe("builder", () => {
     it("can build strings", () => {
         const buffer = new Builder()
-            .addString("foo")
+            .addBulkString("foo")
             .out();
         const exp = "$3\r\nfoo\r\n";
         for (let i = 0; i < exp.length; ++i) {
@@ -21,9 +21,9 @@ describe("builder", () => {
     it("can build arrays", () => {
         const buffer = new Builder()
             .addArray(3)
-            .addString("SET")
-            .addString("foo")
-            .addString("bar")
+            .addBulkString("SET")
+            .addBulkString("foo")
+            .addBulkString("bar")
             .out();
         const exp = "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n";
         for (let i = 0; i < exp.length; ++i) {
